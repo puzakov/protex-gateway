edabba5de377686be0528ed68b49960b5ae2db3a
Artem Puzakov <apuzakov@awardwallet.com>
2019-09-29 12:32:14 +0500

    implementation

--- src/screens/MobileApproove.js
+++ src/screens/MobileApproove.js
@@ -1,23 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 
-export default () => { 
-  return (
-    
-    <div className="ui-mobile">
+class MobileApproove extends Component {
+  state = { step: 1 };
 
-      <header className="ui-mobile__header">
-        <span></span>
-        <h1 className="ui-mobile__header__logo">Протекс</h1>
-      </header>
+  findStepCls = () => {
+    const { step } = this.state;
+    switch (step) {
+      default:
+      case 1:
+        return "";
+      case 2:
+        setTimeout(() => {
+          this.setState({ step: 3 });
+        }, 2000);
+        return "-show-loader";
+      case 3:
+        return "-show-success";
+    }
+  };
 
-      <footer className="ui-mobile__button">
-        <button className="ui-button -forward">Подтвердить</button>
-      </footer>
+  render() {
+    return (
+      <div className="ui-mobile">
+        <header className="ui-mobile__header">
+          <span></span>
+          <h1 className="ui-mobile__header__logo">Протекс</h1>
+        </header>
 
-      <div className="ui-mobile__main">
-        
+        <footer className="ui-mobile__button">
+          <button
+            className="ui-button -forward"
+            onClick={() => {
+              this.setState({ step: 2 }, () => {
+                console.log(
+                  "set step " + this.state.step + " " + this.findStepCls()
+                );
+              });
+            }}
+          >
+            Подтвердить
+          </button>
+        </footer>
+
+        <div className={`ui-mobile__main ${this.findStepCls()}`}>
           <header className="ui-card__main -no-paddings">
-            <div className="ui-card__main__name">Пучкин Вячеслав Витальевич</div>
+            <div className="ui-card__main__name">
+              Пучкин Вячеслав Витальевич
+            </div>
             <div className="ui-kv">
               <label>ИНН:</label>
               <span>590584994669</span>
@@ -25,14 +54,18 @@ export default () => {
           </header>
 
           <div className="ui-card__what">
-            <div className="ui-card__what__name">Заявление на регистрацию ККТ</div>
+            <div className="ui-card__what__name">
+              Заявление на регистрацию ККТ
+            </div>
             <div className="ui-card__what__from">Запрос с nalog.ru</div>
           </div>
-          
+
           <div className="ui-signature">
-            <input type="radio" name="signature" checked/>
+            <input type="radio" name="signature" />
             <span className="ui-signature__background"></span>
-            <span className="ui-signature__name">ИП Пучкин Вячеслав Витальевич</span>
+            <span className="ui-signature__name">
+              ИП Пучкин Вячеслав Витальевич
+            </span>
             <span className="ui-signature__info">
               <span className="ui-kv">
                 <label>ИНН:</label>
@@ -44,10 +77,10 @@ export default () => {
               </span>
             </span>
           </div>
-        
+        </div>
       </div>
-
-    </div>
-
-  );
+    );
+  }
 }
+
+export default MobileApproove;
--- src/screens/MobilePIN.js
+++ src/screens/MobilePIN.js
@@ -1,85 +1,72 @@
-import React, { Component} from "react";
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 
-class MobilePIN extends Component { 
+const buttons = [
+  { digit: 1, text: "" },
+  { digit: 2, text: "ABC" },
+  { digit: 3, text: "DEF" },
+  { digit: 4, text: "GHI" },
+  { digit: 5, text: "JKL" },
+  { digit: 6, text: "MNO" },
+  { digit: 7, text: "PQRS" },
+  { digit: 8, text: "TUV" },
+  { digit: 9, text: "WXYS" },
+  { digit: 0, text: "" }
+];
 
-  componentDidMount() { 
+class MobilePIN extends Component {
+  state = { counter: 0 };
+
+  componentDidMount() {
     const response = fetch(
-      "https://uecolog-api.herokuapp.com/protex/step/3"
+      "https://uecolog-api.herokuapp.com/protex/step/2"
     ).then(response => response.json());
   }
 
-  render() { 
+  render() {
+    const { counter } = this.state;
+    if (counter >= 4) return (<Redirect to="/approve"/>)
     return (
-      
       <div className="ui-mobile">
-  
         <header className="ui-mobile__header">
           <span></span>
           <h1 className="ui-mobile__header__logo">Протекс</h1>
         </header>
-  
+
         <footer className="ui-mobile__footer">
           <button className="ui-mobile__footer__clear">Сбросить PIN</button>
           <button className="ui-mobile__footer__faceid"></button>
         </footer>
-  
+
         <div className="ui-mobile__main">
-          
           <div className="ui-pin">
             <h2 className="ui-pin__title">Введите PIN</h2>
             <div className="ui-pin__progress">
-              <div className="ui-pin__progress__item -active"></div>
-              <div className="ui-pin__progress__item -active"></div>
-              <div className="ui-pin__progress__item"></div>
-              <div className="ui-pin__progress__item"></div>
+              {[0, 1, 2, 3].map(item => {
+                const active = counter > item ? "-active" : "";
+                return (
+                  <div className={`ui-pin__progress__item ${active}`}></div>
+                );
+              })}
             </div>
             <div className="ui-pin__keyboard">
-              <button className="ui-pin__keyboard__item">
-                <div>1</div>
-                <span>&nbsp;</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>2</div>
-                <span>ABC</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>3</div>
-                <span>DEF</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>4</div>
-                <span>GHI</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>5</div>
-                <span>JKL</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>6</div>
-                <span>MNO</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>7</div>
-                <span>PQRS</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>8</div>
-                <span>TUV</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>9</div>
-                <span>WXYZ</span>
-              </button>
-              <button className="ui-pin__keyboard__item">
-                <div>0</div>
-              </button>
+              {buttons.map(({ digit, text }) => {
+                return (
+                  <button
+                    className="ui-pin__keyboard__item"
+                    onClick={() => {
+                      this.setState({ counter: counter + 1 });
+                    }}
+                  >
+                    <div>{digit}</div>
+                    <span>{text}</span>
+                  </button>
+                );
+              })}
             </div>
           </div>
-  
         </div>
-  
       </div>
-  
     );
   }
 }
